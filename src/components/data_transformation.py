9043962ada7d15926c0b26dc3d644fdf61ab7656
Phobos25 <fydte@mail.ru>
2023-05-04 09:09:18 +0900

    Model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -92,7 +92,9 @@ class DataTransformation:
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
-            test_arr = np.c_[input_feature_test_df, np.array(target_feature_test_df)]
+            test_arr = np.c_[
+                input_feature_test_arr, np.array(target_feature_test_df)
+            ]
 
             logging.info(f"Saved preprocessing object")
             save_object(
--- src/utils.py
+++ src/utils.py
@@ -21,27 +21,27 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-    def evaluate_models(X_train, y_train,X_test,y_test,models):
-        try:
-            report = {}
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
 
-            for i in range(len(list(models))):
-                model = list(models.values())[i]              
-              
-                model.fit(X_train,y_train)
+        for i in range(len(list(models))):
+            model = list(models.values())[i]              
+            
+            model.fit(X_train,y_train)
 
-                y_train_pred = model.predict(X_train)
+            y_train_pred = model.predict(X_train)
 
-                y_test_pred = model.predict(X_test)
+            y_test_pred = model.predict(X_test)
 
-                train_model_score = r2_score(y_train, y_train_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
 
-                test_model_score = r2_score(y_test, y_test_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
-                report[list(models.keys())[i]] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
-            return report
+        return report
 
-        except Exception as e:
-            raise CustomException(e, sys)
+    except Exception as e:
+        raise CustomException(e, sys)
     
