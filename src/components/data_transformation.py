1dab7fd9f29fb669cb204fe9a23969914a1dd99b
Phobos25 <fydte@mail.ru>
2023-05-06 08:52:14 +0900

    Prediction Pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -15,7 +15,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifact', "preprocessor.pkl")
+    preprocessor_obj_file_path = os.path.join('artifacts', "preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -74,12 +74,11 @@ class DataTransformation:
             preprocessing_obj = self.get_data_transformer_object()
 
             target_column_name = "math_score"
-            numerical_columns = ['writing_score', 'reading_score']            
 
-            input_feature_train_df = train_df.drop(columns = [target_column_name], axis=1)
+            input_feature_train_df  = train_df.drop(columns = [target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(columns = [target_column_name], axis=1)
+            input_feature_test_df  = test_df.drop(columns = [target_column_name], axis=1)
             target_feature_test_df = test_df[target_column_name]
 
             logging.info(
